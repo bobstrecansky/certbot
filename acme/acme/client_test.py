6fe9e4758acb0cae96c91bf24cfb2ad5d298572a
Bob Strecansky <bob.strecansky@gmail.com>
2017-07-19 18:49:35 -0400

    [#4535] - Unwrap max retries exceeded errors

--- acme/acme/client_test.py
+++ acme/acme/client_test.py
@@ -625,15 +625,11 @@ class ClientNetworkTest(unittest.TestCase):
             # pylint: disable=protected-access
             self.net._send_request('GET', "http://localhost:19123/nonexistent.txt")
 
-        if sys.version_info < (2, 7):
-            except Exception as x:
-            self.assertEqual("Connection Refused", str(x))
-
         except ValueError as y:
-            self.assertIn("Connection refused", str(y))
+            self.assertEqual("Requesting localhost/nonexistent: [Errno 111] Connection refused", str(y))
 
         except requests.exceptions.ConnectionError as z:
-            self.assertIn("Connection refused", str(z))
+            self.assertEqual("Connection refused", str(z))
 
 class ClientNetworkWithMockedResponseTest(unittest.TestCase):
     """Tests for acme.client.ClientNetwork which mock out response."""
